b3ee3517710438d1560a3dd550bae8f2c6678467
Anton Van Assche <vanasscheanton@gmail.com>
2024-11-16 21:10:27 +0100

    Bump version to 0.2.1

--- src/jamjar/__init__.py
+++ src/jamjar/__init__.py
@@ -7,7 +7,7 @@ This module sets up the CLI commands for adding, removing,
 syncing, exporting, and managing playlists in the JamJar database.
 """
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 import json
 import os
