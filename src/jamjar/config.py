d3f51cd672cdd97ff3c26c97b75748ed4b731790
Anton Van Assche <vanasscheanton@gmail.com>
2024-11-17 12:24:21 +0100

    [config] Add support for Windows filesystem paths

--- src/jamjar/config.py
+++ src/jamjar/config.py
@@ -9,8 +9,7 @@ client_secret, redirect_uri, database path, and token file path.
 
 import json
 import os
-
-CONFIG_FILE = os.path.expanduser("~/.jamjar_config.json")
+from pathlib import Path
 
 
 class Config:
@@ -22,10 +21,11 @@ class Config:
     """
 
     def __init__(self):
-        if not os.path.exists(CONFIG_FILE):
-            raise FileNotFoundError(f"Configuration file not found at {CONFIG_FILE}")
+        config_file = self.config_file
+        if not os.path.exists(config_file):
+            raise FileNotFoundError(f"Configuration file not found at {config_file}")
 
-        with open(CONFIG_FILE, "r", encoding="utf-8") as f:
+        with open(config_file, "r", encoding="utf-8") as f:
             self.config_data = json.load(f)
 
     def get(self, key, default=None):
@@ -51,11 +51,28 @@ class Config:
         return self.get("redirect_uri", "http://localhost:5000/callback")
 
     @property
+    def config_file(self):
+        """Retrieve the config file path."""
+        if os.name == "nt":
+            base_dir = Path(os.getenv("APPDATA", r"~/AppData/Roaming")).expanduser()
+            return base_dir / "JamJar" / "config.json"
+
+        return Path.home() / ".jamjar_config.json"
+
+    @property
     def db_path(self):
-        """Retrieve the database path from the config."""
-        return self.get("db_path", "~/.jamjar.db")
+        """Retrieve the database path."""
+        if os.name == "nt":
+            base_dir = Path(os.getenv("APPDATA", r"~/AppData/Roaming")).expanduser()
+            return base_dir / "JamJar" / "jamjar.db"
+
+        return Path.home() / ".jamjar.db"
 
     @property
     def token_file(self):
-        """Retrieve the token file path from the config."""
-        return self.get("token_file", "~/.jamjar_token.json")
+        """Retrieve the token file path."""
+        if os.name == "nt":
+            base_dir = Path(os.getenv("APPDATA", r"~/AppData/Roaming")).expanduser()
+            return base_dir / "JamJar" / "token.json"
+
+        return Path.home() / ".jamjar_token.json"
